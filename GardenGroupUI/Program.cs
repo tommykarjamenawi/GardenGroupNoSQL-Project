e61a57b6e3b6e03e5ffe53165f37084941816b29
Alex Arkhipov <alex.arkhipov.7590@gmail.com>
2021-09-27 14:00:18 +0200

    created a user class with a couple of enums

--- GardenGroupUI/Program.cs
+++ GardenGroupUI/Program.cs
@@ -17,6 +17,8 @@ namespace GardenGroupUI
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new gardenGroupUI());
+
+
         }
     }
 }
