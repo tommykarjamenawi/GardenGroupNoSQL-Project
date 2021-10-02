6e29db4303cbdb4fbe16d27063f58c3a11a802be
B-student <Bingoleley2015@gmail.com>
2021-10-02 22:19:52 +0200

    forms: add user and manage user have been added

---
+++ GardenGroupUI/ManageUser.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace GardenGroupUI
+{
+    partial class ManageUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.ColumnId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ColumnEmail = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ColumnFirstname = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnLastname = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnTickets = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(59, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(251, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User management";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(67, 155);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(118, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(553, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 42);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "&Add new user";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ColumnId,
+            this.ColumnEmail,
+            this.ColumnFirstname,
+            this.columnLastname,
+            this.columnTickets});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(67, 220);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(641, 205);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // ColumnId
+            // 
+            this.ColumnId.Text = "ID";
+            // 
+            // ColumnEmail
+            // 
+            this.ColumnEmail.Text = "Email";
+            this.ColumnEmail.Width = 174;
+            // 
+            // ColumnFirstname
+            // 
+            this.ColumnFirstname.Text = "First name";
+            this.ColumnFirstname.Width = 162;
+            // 
+            // columnLastname
+            // 
+            this.columnLastname.Text = "Last name";
+            this.columnLastname.Width = 155;
+            // 
+            // columnTickets
+            // 
+            this.columnTickets.Text = "# Tickets";
+            this.columnTickets.Width = 86;
+            // 
+            // ManageUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "ManageUser";
+            this.Text = "ManageUser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader ColumnId;
+        private System.Windows.Forms.ColumnHeader ColumnEmail;
+        private System.Windows.Forms.ColumnHeader ColumnFirstname;
+        private System.Windows.Forms.ColumnHeader columnLastname;
+        private System.Windows.Forms.ColumnHeader columnTickets;
+    }
+}
