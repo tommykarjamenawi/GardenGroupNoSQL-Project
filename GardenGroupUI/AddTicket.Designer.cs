883df7458062e41dfddbb11b9e4c6801299444cf
paxer2k <70654308+paxer2k@users.noreply.github.com>
2021-10-09 02:06:51 +0200

    last changes for today (maybe..)

--- GardenGroupUI/AddTicket.Designer.cs
+++ GardenGroupUI/AddTicket.Designer.cs
@@ -52,9 +52,10 @@ namespace GardenGroupUI
             // btnAdd
             // 
             this.btnAdd.BackColor = System.Drawing.Color.MediumSeaGreen;
-            this.btnAdd.Location = new System.Drawing.Point(435, 366);
+            this.btnAdd.Location = new System.Drawing.Point(326, 329);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(103, 34);
+            this.btnAdd.Size = new System.Drawing.Size(77, 28);
             this.btnAdd.TabIndex = 33;
             this.btnAdd.Text = "&Add ticket";
             this.btnAdd.UseVisualStyleBackColor = false;
@@ -64,9 +65,10 @@ namespace GardenGroupUI
             // 
             this.btnCancel.BackColor = System.Drawing.Color.MediumSeaGreen;
             this.btnCancel.ForeColor = System.Drawing.SystemColors.MenuText;
-            this.btnCancel.Location = new System.Drawing.Point(326, 366);
+            this.btnCancel.Location = new System.Drawing.Point(244, 329);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(103, 34);
+            this.btnCancel.Size = new System.Drawing.Size(77, 28);
             this.btnCancel.TabIndex = 32;
             this.btnCancel.Text = "&Cancel";
             this.btnCancel.UseVisualStyleBackColor = false;
@@ -76,143 +78,160 @@ namespace GardenGroupUI
             // 
             this.cmbTypeOfPriority.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbTypeOfPriority.FormattingEnabled = true;
-            this.cmbTypeOfPriority.Location = new System.Drawing.Point(325, 320);
+            this.cmbTypeOfPriority.Location = new System.Drawing.Point(244, 198);
+            this.cmbTypeOfPriority.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cmbTypeOfPriority.Name = "cmbTypeOfPriority";
-            this.cmbTypeOfPriority.Size = new System.Drawing.Size(213, 24);
+            this.cmbTypeOfPriority.Size = new System.Drawing.Size(161, 21);
             this.cmbTypeOfPriority.TabIndex = 30;
             // 
             // cmbTypeOfIncident
             // 
             this.cmbTypeOfIncident.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbTypeOfIncident.FormattingEnabled = true;
-            this.cmbTypeOfIncident.Location = new System.Drawing.Point(325, 280);
+            this.cmbTypeOfIncident.Location = new System.Drawing.Point(244, 128);
+            this.cmbTypeOfIncident.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cmbTypeOfIncident.Name = "cmbTypeOfIncident";
-            this.cmbTypeOfIncident.Size = new System.Drawing.Size(213, 24);
+            this.cmbTypeOfIncident.Size = new System.Drawing.Size(161, 21);
             this.cmbTypeOfIncident.TabIndex = 29;
             // 
             // txtSubject
             // 
-            this.txtSubject.Location = new System.Drawing.Point(326, 118);
+            this.txtSubject.Location = new System.Drawing.Point(244, 96);
+            this.txtSubject.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtSubject.Name = "txtSubject";
-            this.txtSubject.Size = new System.Drawing.Size(213, 22);
+            this.txtSubject.Size = new System.Drawing.Size(161, 20);
             this.txtSubject.TabIndex = 28;
             // 
             // txtDescription
             // 
-            this.txtDescription.Location = new System.Drawing.Point(325, 156);
+            this.txtDescription.Location = new System.Drawing.Point(244, 266);
+            this.txtDescription.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtDescription.Multiline = true;
             this.txtDescription.Name = "txtDescription";
-            this.txtDescription.Size = new System.Drawing.Size(213, 22);
+            this.txtDescription.Size = new System.Drawing.Size(161, 59);
             this.txtDescription.TabIndex = 27;
             // 
             // txtPhoneNumber
             // 
-            this.txtPhoneNumber.Location = new System.Drawing.Point(868, 161);
+            this.txtPhoneNumber.Location = new System.Drawing.Point(651, 131);
+            this.txtPhoneNumber.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtPhoneNumber.Name = "txtPhoneNumber";
-            this.txtPhoneNumber.Size = new System.Drawing.Size(213, 22);
+            this.txtPhoneNumber.Size = new System.Drawing.Size(161, 20);
             this.txtPhoneNumber.TabIndex = 26;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(157, 159);
+            this.label8.Location = new System.Drawing.Point(118, 266);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(83, 17);
+            this.label8.Size = new System.Drawing.Size(63, 13);
             this.label8.TabIndex = 24;
             this.label8.Text = "Description:";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(157, 201);
+            this.label7.Location = new System.Drawing.Point(118, 69);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(42, 17);
+            this.label7.Size = new System.Drawing.Size(99, 13);
             this.label7.TabIndex = 23;
-            this.label7.Text = "Date:";
+            this.label7.Text = "Date/time reported:";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(157, 327);
+            this.label6.Location = new System.Drawing.Point(118, 204);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(107, 17);
+            this.label6.Size = new System.Drawing.Size(79, 13);
             this.label6.TabIndex = 22;
             this.label6.Text = "Type of priority:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(157, 240);
+            this.label5.Location = new System.Drawing.Point(118, 236);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(68, 17);
+            this.label5.Size = new System.Drawing.Size(52, 13);
             this.label5.TabIndex = 21;
             this.label5.Text = "Deadline:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(157, 287);
+            this.label4.Location = new System.Drawing.Point(118, 133);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(113, 17);
+            this.label4.Size = new System.Drawing.Size(86, 13);
             this.label4.TabIndex = 20;
             this.label4.Text = "Type of incident:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(159, 123);
+            this.label3.Location = new System.Drawing.Point(119, 100);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(59, 17);
+            this.label3.Size = new System.Drawing.Size(98, 13);
             this.label3.TabIndex = 19;
-            this.label3.Text = "Subject:";
+            this.label3.Text = "Subject of incident:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(157, 87);
+            this.label2.Location = new System.Drawing.Point(118, 169);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(42, 17);
+            this.label2.Size = new System.Drawing.Size(91, 13);
             this.label2.TabIndex = 18;
-            this.label2.Text = "User:";
+            this.label2.Text = "Reported by user:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(155, 28);
+            this.label1.Location = new System.Drawing.Point(116, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(242, 31);
+            this.label1.Size = new System.Drawing.Size(197, 26);
             this.label1.TabIndex = 17;
             this.label1.Text = "Create new ticket";
             // 
             // dtpDate
             // 
-            this.dtpDate.Location = new System.Drawing.Point(325, 196);
+            this.dtpDate.Location = new System.Drawing.Point(244, 65);
+            this.dtpDate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dtpDate.Name = "dtpDate";
-            this.dtpDate.Size = new System.Drawing.Size(213, 22);
+            this.dtpDate.Size = new System.Drawing.Size(161, 20);
             this.dtpDate.TabIndex = 34;
             // 
             // dtpDeadline
             // 
-            this.dtpDeadline.Location = new System.Drawing.Point(325, 235);
+            this.dtpDeadline.Location = new System.Drawing.Point(244, 232);
+            this.dtpDeadline.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dtpDeadline.Name = "dtpDeadline";
-            this.dtpDeadline.Size = new System.Drawing.Size(213, 22);
+            this.dtpDeadline.Size = new System.Drawing.Size(161, 20);
             this.dtpDeadline.TabIndex = 35;
             // 
             // cmbUser
             // 
             this.cmbUser.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbUser.FormattingEnabled = true;
-            this.cmbUser.Location = new System.Drawing.Point(325, 80);
+            this.cmbUser.Location = new System.Drawing.Point(244, 163);
+            this.cmbUser.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cmbUser.Name = "cmbUser";
-            this.cmbUser.Size = new System.Drawing.Size(213, 24);
+            this.cmbUser.Size = new System.Drawing.Size(161, 21);
             this.cmbUser.TabIndex = 36;
             // 
             // AddTicket
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(217)))), ((int)(((byte)(112)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.cmbUser);
             this.Controls.Add(this.dtpDeadline);
             this.Controls.Add(this.dtpDate);
@@ -231,6 +250,7 @@ namespace GardenGroupUI
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "AddTicket";
             this.Text = "AddTicket";
             this.Load += new System.EventHandler(this.AddTicket_Load);
