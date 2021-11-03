043d7a90a586ca0105be709094235faf43ca1f1b
Alex Arkhipov <alex.arkhipov.7590@gmail.com>
2021-11-03 03:16:32 +0100

    yes

--- GardenGroupUI/TicketOverviewStatistics.Designer.cs
+++ GardenGroupUI/TicketOverviewStatistics.Designer.cs
@@ -59,22 +59,21 @@ namespace GardenGroupUI
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(-2, 4);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(-3, 5);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(344, 46);
+            this.label1.Size = new System.Drawing.Size(438, 59);
             this.label1.TabIndex = 0;
             this.label1.Text = "Current incidents";
             // 
             // btnShowTickets
             // 
             this.btnShowTickets.BackColor = System.Drawing.Color.MediumSeaGreen;
-            this.btnShowTickets.Location = new System.Drawing.Point(634, 9);
-            this.btnShowTickets.Margin = new System.Windows.Forms.Padding(2);
+            this.btnShowTickets.Location = new System.Drawing.Point(845, 11);
+            this.btnShowTickets.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnShowTickets.Name = "btnShowTickets";
-            this.btnShowTickets.Size = new System.Drawing.Size(113, 41);
+            this.btnShowTickets.Size = new System.Drawing.Size(151, 50);
             this.btnShowTickets.TabIndex = 1;
-            this.btnShowTickets.Text = "SHOW TICKETS";
+            this.btnShowTickets.Text = "&Show tickets";
             this.btnShowTickets.UseVisualStyleBackColor = false;
             this.btnShowTickets.Click += new System.EventHandler(this.btnShowTickets_Click);
             // 
@@ -82,10 +81,9 @@ namespace GardenGroupUI
             // 
             this.lblUnsolvedTickets.AutoSize = true;
             this.lblUnsolvedTickets.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblUnsolvedTickets.Location = new System.Drawing.Point(93, 199);
-            this.lblUnsolvedTickets.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblUnsolvedTickets.Location = new System.Drawing.Point(124, 245);
             this.lblUnsolvedTickets.Name = "lblUnsolvedTickets";
-            this.lblUnsolvedTickets.Size = new System.Drawing.Size(42, 44);
+            this.lblUnsolvedTickets.Size = new System.Drawing.Size(51, 55);
             this.lblUnsolvedTickets.TabIndex = 2;
             this.lblUnsolvedTickets.Text = "_";
             // 
@@ -93,10 +91,9 @@ namespace GardenGroupUI
             // 
             this.lblTotalTickets.AutoSize = true;
             this.lblTotalTickets.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTotalTickets.Location = new System.Drawing.Point(221, 199);
-            this.lblTotalTickets.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTotalTickets.Location = new System.Drawing.Point(295, 245);
             this.lblTotalTickets.Name = "lblTotalTickets";
-            this.lblTotalTickets.Size = new System.Drawing.Size(42, 44);
+            this.lblTotalTickets.Size = new System.Drawing.Size(51, 55);
             this.lblTotalTickets.TabIndex = 3;
             this.lblTotalTickets.Text = "_";
             // 
@@ -104,10 +101,9 @@ namespace GardenGroupUI
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(164, 199);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(219, 245);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(32, 44);
+            this.label2.Size = new System.Drawing.Size(38, 55);
             this.label2.TabIndex = 4;
             this.label2.Text = "/";
             // 
@@ -115,10 +111,9 @@ namespace GardenGroupUI
             // 
             this.lblPastDeadline.AutoSize = true;
             this.lblPastDeadline.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPastDeadline.Location = new System.Drawing.Point(536, 199);
-            this.lblPastDeadline.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPastDeadline.Location = new System.Drawing.Point(715, 245);
             this.lblPastDeadline.Name = "lblPastDeadline";
-            this.lblPastDeadline.Size = new System.Drawing.Size(42, 44);
+            this.lblPastDeadline.Size = new System.Drawing.Size(51, 55);
             this.lblPastDeadline.TabIndex = 5;
             this.lblPastDeadline.Text = "_";
             // 
@@ -126,10 +121,9 @@ namespace GardenGroupUI
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(84, 132);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(112, 162);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(212, 25);
+            this.label3.Size = new System.Drawing.Size(261, 31);
             this.label3.TabIndex = 6;
             this.label3.Text = "Unsolved incidents";
             // 
@@ -137,10 +131,9 @@ namespace GardenGroupUI
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(112, 168);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Location = new System.Drawing.Point(149, 207);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(148, 16);
+            this.label5.Size = new System.Drawing.Size(184, 20);
             this.label5.TabIndex = 8;
             this.label5.Text = "Unsolved / Total tickets";
             // 
@@ -148,10 +141,9 @@ namespace GardenGroupUI
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(432, 132);
-            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Location = new System.Drawing.Point(576, 162);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(256, 25);
+            this.label6.Size = new System.Drawing.Size(315, 31);
             this.label6.TabIndex = 9;
             this.label6.Text = "Incidents past deadline";
             // 
@@ -159,10 +151,9 @@ namespace GardenGroupUI
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(458, 168);
-            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Location = new System.Drawing.Point(611, 207);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(208, 16);
+            this.label7.Size = new System.Drawing.Size(261, 20);
             this.label7.TabIndex = 10;
             this.label7.Text = "Amount of incidents past deadline";
             // 
@@ -173,7 +164,8 @@ namespace GardenGroupUI
             this.chartUnresolvedTickets.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             this.chartUnresolvedTickets.Legends.Add(legend1);
-            this.chartUnresolvedTickets.Location = new System.Drawing.Point(25, 277);
+            this.chartUnresolvedTickets.Location = new System.Drawing.Point(33, 341);
+            this.chartUnresolvedTickets.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.chartUnresolvedTickets.Name = "chartUnresolvedTickets";
             series1.ChartArea = "ChartArea1";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Doughnut;
@@ -181,7 +173,7 @@ namespace GardenGroupUI
             series1.Legend = "Legend1";
             series1.Name = "Unresolved incidents";
             this.chartUnresolvedTickets.Series.Add(series1);
-            this.chartUnresolvedTickets.Size = new System.Drawing.Size(331, 278);
+            this.chartUnresolvedTickets.Size = new System.Drawing.Size(441, 342);
             this.chartUnresolvedTickets.TabIndex = 11;
             this.chartUnresolvedTickets.Text = "chart1";
             title1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -199,7 +191,8 @@ namespace GardenGroupUI
             this.chartIncidentsPastDeadline.ChartAreas.Add(chartArea2);
             legend2.Name = "Legend1";
             this.chartIncidentsPastDeadline.Legends.Add(legend2);
-            this.chartIncidentsPastDeadline.Location = new System.Drawing.Point(400, 277);
+            this.chartIncidentsPastDeadline.Location = new System.Drawing.Point(533, 341);
+            this.chartIncidentsPastDeadline.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.chartIncidentsPastDeadline.Name = "chartIncidentsPastDeadline";
             series2.ChartArea = "ChartArea1";
             series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Doughnut;
@@ -207,7 +200,7 @@ namespace GardenGroupUI
             series2.Legend = "Legend1";
             series2.Name = "Past deadline";
             this.chartIncidentsPastDeadline.Series.Add(series2);
-            this.chartIncidentsPastDeadline.Size = new System.Drawing.Size(331, 278);
+            this.chartIncidentsPastDeadline.Size = new System.Drawing.Size(441, 342);
             this.chartIncidentsPastDeadline.TabIndex = 13;
             this.chartIncidentsPastDeadline.Text = "chart1";
             title3.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -220,10 +213,10 @@ namespace GardenGroupUI
             // 
             // TicketOverviewStatistics
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(217)))), ((int)(((byte)(112)))));
-            this.ClientSize = new System.Drawing.Size(758, 567);
+            this.ClientSize = new System.Drawing.Size(1011, 698);
             this.Controls.Add(this.chartIncidentsPastDeadline);
             this.Controls.Add(this.chartUnresolvedTickets);
             this.Controls.Add(this.label7);
@@ -236,7 +229,7 @@ namespace GardenGroupUI
             this.Controls.Add(this.lblUnsolvedTickets);
             this.Controls.Add(this.btnShowTickets);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "TicketOverviewStatistics";
             this.Text = "TicketOverviewStatistics";
             this.Load += new System.EventHandler(this.TicketOverviewStatistics_Load);
